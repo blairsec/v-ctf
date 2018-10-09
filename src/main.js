68faaccda87b565b1534c495fa4b724a5ad42230
Kevin Higgs <github@kevinhiggs.com>
2018-10-09 18:55:58 -0400

    header authentication

--- src/main.js
+++ src/main.js
@@ -14,55 +14,28 @@ Vue.mixin({
       return marked(text)
     },
     post (route, data, competition) {
-      var _csrf = this.randomString(64).replace(/[;, ]/g, '')
-      document.cookie = '_csrf=' + _csrf + '; Domain=' + window.location.hostname
-      return axios.post(config.API_URL + (competition === false ? '' : ('/competitions/' + this.$store.competition.id)) + route,
-        Object.assign(data, { _csrf: _csrf }),
-        { withCredentials: true })
+      return axios.post(config.API_URL + (competition === false ? '' : ('/competitions/' + this.$store.competition.id)) + route, data, this.$store.competition ? { headers: { Authorization: 'Token ' + localStorage['competition_' + this.$store.competition.id] } } : {})
     },
     get (route, competition) {
-      return axios.get(config.API_URL + (competition === false ? '' : ('/competitions/' + this.$store.competition.id)) + route, { withCredentials: true })
+      return axios.get(config.API_URL + (competition === false ? '' : ('/competitions/' + this.$store.competition.id)) + route, this.$store.competition ? { headers: { Authorization: 'Token ' + localStorage['competition_' + this.$store.competition.id] } } : {})
     },
     patch (route, data, competition) {
-      var _csrf = this.randomString(64).replace(/[;, ]/g, '')
-      document.cookie = '_csrf=' + _csrf + '; Domain=' + window.location.hostname
-      return axios.patch(config.API_URL + (competition === false ? '' : ('/competitions/' + this.$store.competition.id)) + route,
-        Object.assign(data, { _csrf: _csrf }),
-        { withCredentials: true })
+      return axios.patch(config.API_URL + (competition === false ? '' : ('/competitions/' + this.$store.competition.id)) + route, data, this.$store.competition ? { headers: { Authorization: 'Token ' + localStorage['competition_' + this.$store.competition.id] } } : {})
     },
     put (route, data, competition) {
-      var _csrf = this.randomString(64).replace(/[;, ]/g, '')
-      document.cookie = '_csrf=' + _csrf + '; Domain=' + window.location.hostname
-      return axios.put(config.API_URL + (competition === false ? '' : ('/competitions/' + this.$store.competition.id)) + route,
-        Object.assign(data, { _csrf: _csrf }),
-        { withCredentials: true })
+      return axios.put(config.API_URL + (competition === false ? '' : ('/competitions/' + this.$store.competition.id)) + route, data, this.$store.competition ? { headers: { Authorization: 'Token ' + localStorage['competition_' + this.$store.competition.id] } } : {})
     },
     deleteRequest (route, competition) {
-      var _csrf = this.randomString(64).replace(/[;, ]/g, '')
-      document.cookie = '_csrf=' + _csrf + '; Domain=' + window.location.hostname
-      return axios.delete(config.API_URL + (competition === false ? '' : ('/competitions/' + this.$store.competition.id)) + route,
-        { params: { _csrf: _csrf }, withCredentials: true })
-    },
-    randomString (length) {
-      var string = ''
-      var crypto = window.crypto || window.msCrypto
-      var values = new Uint32Array(length)
-      crypto.getRandomValues(values)
-      for (var i = 0; i < length; i++) {
-        string += String.fromCharCode((values[i] % (127 - 32)) + 32)
-      }
-      return string
+      return axios.delete(config.API_URL + (competition === false ? '' : ('/competitions/' + this.$store.competition.id)) + route, this.$store.competition ? { headers: { Authorization: 'Token ' + localStorage['competition_' + this.$store.competition.id] } } : {})
     },
     logout () {
       localStorage.removeItem('competition_' + this.$store.competition.id)
-      document.cookie = 'token =; Max-Age=0; Domain=' + window.location.hostname
       this.reload()
       this.alert('Success!', 'You have been logged out.', 'success')
     },
     updateCompetition (competition) {
       this.$store.competition = competition
       localStorage.competition = competition.id
-      if (!this.$store.user.admin && this.$store.competition) document.cookie = 'token=' + localStorage['competition_' + this.$store.competition.id] + '; Domain=' + window.location.hostname
       this.reload()
     },
     alert (title, message, type, duration) {
@@ -93,12 +66,10 @@ Vue.mixin({
         if ((this.$store.competition === null || this.$store.competition.id === res.data.competition.id) && res.data.competition) this.$store.competition = res.data.competition
         else if (this.$store.user.admin === true) this.$store.competition = this.$store.competition || this.$store.competitions.filter(c => c.id === parseInt(localStorage.competition))[0] || this.$store.competitions[this.$store.competitions.length - 1] || null
         else this.$store.user = emptyUser
-        if (!this.$store.user.admin && this.$store.competition) document.cookie = 'token=' + localStorage['competition_' + this.$store.competition.id] + '; Domain=' + window.location.hostname
         this.$store.competitionLoaded = true
         this.$store.loaded = true
       }.bind(this)).catch(function (data) {
         this.$store.competition = this.$store.competition || this.$store.competitions.filter(c => c.id === parseInt(localStorage.competition))[0] || this.$store.competitions[this.$store.competitions.length - 1]
-        if (!this.$store.user.admin && this.$store.competition) document.cookie = 'token=' + localStorage['competition_' + this.$store.competition.id] + '; Domain=' + window.location.hostname
         this.$store.competitionLoaded = true
         this.$store.loaded = true
         this.$store.user = emptyUser
